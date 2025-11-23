4bfd116778ff2888f3c598f19465ab218406833a
alba-636 <29271954+alba-636@users.noreply.github.com>
2025-11-23 17:38:56 +0100

    feat: more service example

--- start_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-scripts=("$SCRIPT_DIR/start_kismet.sh" "$SCRIPT_DIR/target_detector.sh")
-
-for script in scripts; do
-    echo "$script"
-done | xargs -P 2 -I {} /bin/bash "{}"
--- start_target_detector.sh
+++ start_target_detector.sh
@@ -2,4 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Start Target Detector
 /usr/bin/python3 $SCRIPT_DIR/src/target_detector.py --config-path $SCRIPT_DIR/config/config.json
