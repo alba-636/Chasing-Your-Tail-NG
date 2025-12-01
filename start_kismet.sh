761fb7d86c67e6a08d922205c223f294aacbf8e9
alba-636 <29271954+alba-636@users.noreply.github.com>
2025-12-01 15:44:11 +0100

    chore: set monitor mode manually

--- start_kismet.sh
+++ start_kismet.sh
@@ -2,5 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Set wlan1 in monitor mode
+/usr/bin/sudo /usr/sbin/airmon-ng start wlan1
+
 # Start kismet
 /usr/bin/sudo /usr/bin/kismet --override=$SCRIPT_DIR/config/kismet.conf
